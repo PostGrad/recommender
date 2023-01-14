1af5900931c63a0379df578e62efaeb758d4a48a
Pranay <pranay.patel@gupshup.io>
2023-01-14 14:22:58 +0530

    added All button to display all songs

---
+++ README.md
@@ -0,0 +1,7 @@
+# recommender
+
+a music catalog that lets you go through different music genres.
+
+Project Link - https://d2cmbb.csb.app/
+
+Created with CodeSandbox - https://codesandbox.io/s/recommender-d2cmbb
---
+++ src/App.js
@@ -0,0 +1,158 @@
+import React, { useState } from "react";
+import { GeneratedIdentifierFlags } from "typescript";
+import "./styles.css";
+
+const musicCatalog = {
+  Hiphop: [
+    {
+      name: "paathshala",
+      description: "A modern world but still old school hip-hop song.",
+      artist: "spitfire",
+      rating: "4.8/5",
+      link: "https://www.youtube.com/watch?v=_bURZgrnmoE",
+    },
+    {
+      name: "kohinoor",
+      description: "A gem of a rap written by a Kohinoor himself.",
+      artist: "divine",
+      rating: "4.4/5",
+      link: "https://www.youtube.com/watch?v=7dt9LvdSdIA",
+    },
+    {
+      name: "mawali qawali",
+      description: "A fun beat song, will definitely make you dance!",
+      artist: "mojo jojo",
+      rating: "5/5",
+      link: "https://www.youtube.com/watch?v=yoyKqkHqGcc",
+    },
+  ],
+  Qawwali: [
+    {
+      name: "saadagi to hamari zara dekhiye",
+      description: "Well, this one will make you remember people.",
+      artist: "nusrat fateh ali khan",
+      rating: "5/5",
+      link: "https://www.youtube.com/watch?v=AMUGle-YeVI",
+    },
+    {
+      name: "kali kali zulfon ke phande na dalo",
+      description: "And this is gonna make you repel some people.",
+      artist: "nusrat fateh ali khan",
+      rating: "5/5",
+      link: "https://www.youtube.com/watch?v=lsqsggtTZfs",
+    },
+    {
+      name: "kun faya kun",
+      description: "A song which you will always need!",
+      artist: "a. r. rahman",
+      rating: "5/5",
+      link: "https://www.youtube.com/watch?v=T94PHkuydcw",
+    },
+  ],
+  Devotional: [
+    {
+      name: "shree ganeshay dheemahi",
+      description: "This song needs no description. This song gives you peace, gives you confidence, etc.",
+      artist: "shankar mahadevan",
+      rating: "5/5",
+      link: "https://www.youtube.com/watch?v=H8ezdkxrx0c",
+    },
+    {
+      name: "hanuman chalisa",
+      description: "This one's for the OG Indian Superhero. This song describes the life of Hanuman along with the events.",
+      artist: "shankar mahadevan",
+      rating: "5/5",
+      link: "https://www.youtube.com/watch?v=r8ctRBDuE8Y",
+    },
+    {
+      name: "o paalanhaare",
+      description: "This song speaks to people in difficulty by asking Lord Krishna to help them.",
+      artist: "a. r. rahman",
+      rating: "5/5",
+      link: "https://www.youtube.com/watch?v=kbMinfmC3E0",
+    },
+  ],
+};
+
+var genres = Object.keys(musicCatalog);
+genres = ["All", ...genres];
+
+export default function App() {
+  const [selected, setSelected] = useState("Devotional");
+  const [catalog, setCatalog] = useState(musicCatalog["Devotional"]);
+  function genreClickHandler(genre) {
+    setSelected(genre);
+    if (genre === "All") {
+      let tempCatalog = [];
+      for (let key of Object.keys(musicCatalog)) {
+        let tempArr = musicCatalog[key].map((item) => item);
+        tempCatalog = [...tempArr, ...tempCatalog];
+      }
+      console.log("generated tempCatalogArr===>>>", tempCatalog);
+      setCatalog(tempCatalog);
+    } else {
+      setCatalog(musicCatalog[genre]);
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1>
+        <span role="img" aria-label="musicRill">
+          🎵
+        </span>{" "}
+        music-catalog
+      </h1>
+      <p>Explore your Genre.</p>
+
+      {genres.map((genre) => {
+        return (
+          <button
+            onClick={() => genreClickHandler(genre)}
+            key={genre}
+            style={{
+              cursor: "pointer",
+              background: "rgb(229, 231, 235)",
+              marginRight: "1rem",
+              borderRadius: "0.5rem",
+              padding: "0.5rem 1rem",
+              border: "1px solid black",
+              margin: "1rem 0.3rem",
+              backgroundColor: selected === genre ? "grey" : "white",
+            }}
+            className="bigButton"
+          >
+            {genre}
+          </button>
+        );
+      })}
+
+      <hr />
+
+      <ul style={{ paddingInlineStart: "0px", textAlign: "left" }}>
+        {catalog.map((song) => {
+          return (
+            <a href={song.link} style={{ textDecoration: "none" }}>
+              <li
+                style={{
+                  listStyleType: "none",
+                  padding: "1rem",
+                  border: "1px solid #D1D5DB",
+                  borderRadius: "0.5rem",
+                  margin: "1rem 0rem",
+                  fontSize: "larger",
+                }}
+                key={song.name}
+              >
+                <div style={{ fontSize: "1.5rem", padding: "0.2rem" }}>{song.name}</div>
+                <div style={{ fontSize: "1rem", padding: "0.1rem" }}>description: {song.description}</div>
+                <div style={{ fontSize: "0.8rem", padding: "0.1rem 0.2rem" }}>artist: {song.artist}</div>
+                <div style={{ fontSize: "1rem", padding: "0 0.2rem" }}>rating: {song.rating}</div>
+              </li>
+            </a>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
